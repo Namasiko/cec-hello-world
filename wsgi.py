2e38601ff262c7d52dcbce20c9b97e1f33318538
Namasiko <36294228+Namasiko@users.noreply.github.com>
2018-02-25 12:31:24 +0200

    tes

--- wsgi.py
+++ wsgi.py
@@ -7,8 +7,6 @@ application = Flask(__name__)
 def hello():
     
     return "Hello World! Greetings from "+socket.gethostname()+"\n"+"TEST"
-    return window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, onFileSystemSuccess, fail);
-
 
 if __name__ == "__main__":
     application.run()
