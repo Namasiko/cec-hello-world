4047810783cadf71d0507b91b49ca9663770f48d
Namasiko <36294228+Namasiko@users.noreply.github.com>
2018-02-25 12:55:27 +0200

    help

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,7 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
     
-    return "Hello World! Greetings from "+socket.gethostname()+"\n"+"TEST"+"timestamp":2016-09-08T05:04:46Z"
+    return "Hello World! Greetings from "+socket.gethostname()+"\n"+"TEST"
 
 
 if __name__ == "__main__":
