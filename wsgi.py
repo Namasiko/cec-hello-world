9c690766967d61d825620703bee9c635537c4866
Namasiko <36294228+Namasiko@users.noreply.github.com>
2018-02-25 13:13:47 +0200

    test

--- wsgi.py
+++ wsgi.py
@@ -7,11 +7,7 @@ application = Flask(__name__)
 def hello():
     
     return "Hello World! Greetings from "+socket.gethostname()+"\n"+"TEST"
-    import time;
-	ts = time.time()
-	print(ts)
-	# 1519322772.43	
-return (ts)
+ 
 
 if __name__ == "__main__":
     application.run()
