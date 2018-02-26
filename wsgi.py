91316bf844d0fd646972f980799f0c18c36c3f9d
Namasiko <36294228+Namasiko@users.noreply.github.com>
2018-02-26 23:45:36 +0200

    he

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,8 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
     
-    return "Hello World! Greetings from "+socket.gethostname()+"\n"+ "hello" 
+    with open("/MNT/log_file.log") as my_file:
+	my_file.write(socket.gethostname() + " " + str(time.time()) + "\n")
 
 	if __name__ == "__main__":
     application.run()
