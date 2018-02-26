d234b24097ca9dc58bd134c8d82d2edb243929c5
Namasiko <36294228+Namasiko@users.noreply.github.com>
2018-02-27 01:10:00 +0200

    test

--- wsgi.py
+++ wsgi.py
@@ -5,7 +5,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World! Greetings from "+socket.gethostname()+"\n"
+    return "Hello World! Greetings from "+socket.gethostname()+"\n" + "Test"
 	with open("/mnt/log_file.log") as my_file:
     my_file.write(socket.gethostname() + "  " + str(time.time()) + "\n")
 
