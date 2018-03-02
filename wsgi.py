bdb58da812d436f368f63890e31b653e760e0681
Namasiko <36294228+Namasiko@users.noreply.github.com>
2018-03-02 15:28:39 +0200

    more fixes

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
 import socket
+import time
 from flask import Flask
 
 application = Flask(__name__)
@@ -6,6 +7,7 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
     with open("/mnt/log_file.log", "a") as my_file:
+    #with open("log_file.log", "a") as my_file:
         my_file.write(socket.gethostname() + "  " + str(time.time()) + "\n")
     return "test"
 
