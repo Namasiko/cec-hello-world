116ccc75793eaeb29d6949a6d94649c65a26883a
Namasiko <36294228+Namasiko@users.noreply.github.com>
2018-03-02 16:04:24 +0200

    try

--- wsgi.py
+++ wsgi.py
@@ -6,9 +6,9 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    with open("/mnt/log_file.log", "a") as my_file:
+    #with open("/mnt/log_file.log", "a") as my_file:
     #with open("log_file.log", "a") as my_file:
-        my_file.write(socket.gethostname() + "  " + str(time.time()) + "\n")
+        #my_file.write(socket.gethostname() + "  " + str(time.time()) + "\n")
 
     with open("/mnt/log_file.log") as my_file:
          for line in my_file:
