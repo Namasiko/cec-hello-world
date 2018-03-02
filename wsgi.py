f1104d739044415f87748eb1d2e9a1816b00777b
Namasiko <36294228+Namasiko@users.noreply.github.com>
2018-03-02 15:56:22 +0200

    try

--- wsgi.py
+++ wsgi.py
@@ -11,8 +11,8 @@ def hello():
         my_file.write(socket.gethostname() + "  " + str(time.time()) + "\n")
 
     with open("/mnt/log_file.log") as my_file:
-        for line in my_file:
-        hello_string += line + "<br>"
+         for line in my_file:
+            hello_string += line + "<br>"
 
     return "Hello World! Greetings from "+socket.gethostname()+"\n"+"TEST"
 
