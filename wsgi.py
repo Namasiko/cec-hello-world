7cbdb13afe9e24c163096a32a4ab727b1f633522
Namasiko <36294228+Namasiko@users.noreply.github.com>
2018-03-02 15:31:31 +0200

    try

--- wsgi.py
+++ wsgi.py
@@ -9,6 +9,10 @@ def hello():
     with open("/mnt/log_file.log", "a") as my_file:
     #with open("log_file.log", "a") as my_file:
         my_file.write(socket.gethostname() + "  " + str(time.time()) + "\n")
+
+    with open("/mnt/log_file.log") as my_file:
+    for line in my_file:
+        hello_string += line + "<br>"
     return "test"
 
 if __name__ == "__main__":
